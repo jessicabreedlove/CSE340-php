ebe46ac399400e1b3e86f66d4dada95504d122bd
jessicabreedlove <jessicabreedlove@gmail.com>
2022-04-28 17:43:26 -0600

    correct file path?

---
+++ htdocs/phpmotors/common/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Motors Template - Jessica Reece</title>
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/base.css">
+  <link rel="stylesheet" href="../css/medium.css">
+  <link rel="stylesheet" href="../css/large.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="top">
+    <img src="../images/site/logo.png" alt="php motors logo">
+    <a class="account" href="http://" target="_blank">My Account</a>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="http://">Home</a></li>
+        <li><a href="http://">Classic</a></li>
+        <li><a href="http://">Sports</a></li>
+        <li><a href="http://">SUV</a></li>
+        <li><a href="http://">Trucks</a></li>
+        <li><a href="http://">Used</a></li>
+      </ul>
+    </nav>
+    <h1>Content Title Here</h1>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    &copy; PHP Motors, All rights reserved.<br>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br>
+    <?php 
+    echo "Last Updated: " . 
+    date("F d Y");
+    ?>
+
+  </footer>
+  
+</body>
+</html>
